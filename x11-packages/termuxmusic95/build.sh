c014d04d7be341134b3b83c5108d910bd411efc0
alexdvn <alexdgames01@gmail.com>
2025-11-23 19:37:20 -0500

    Add build dependencies for TermuxMusic95

--- x11-packages/termuxmusic95/build.sh
+++ x11-packages/termuxmusic95/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.2a
 TERMUX_PKG_SRCURL=https://github.com/quydev-fs/TermuxMusic95/archive/refs/tags/v$TERMUX_PKG_VERSION.zip
 TERMUX_PKG_DEPENDS="gtk3, gstreamer"
+TERMUX_PKG_BUILD_DEPENDS="clang, make, pkg-config"
