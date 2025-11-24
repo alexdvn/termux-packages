973a8e58581295be5194362579566644f59f99bd
alexdvn <alexdgames01@gmail.com>
2025-11-23 19:34:03 -0500

    Made a half-assed build.sh so when the page resets I won't lost my progress in writing the file

---
+++ x11-packages/termuxmusic95/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/quydev-fs/TermuxMusic95
+TERMUX_PKG_DESCRIPTION="A simple WinAMP 2.X inspired Music player built specifically for termux"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.2.2a
+TERMUX_PKG_SRCURL=https://github.com/quydev-fs/TermuxMusic95/archive/refs/tags/v$TERMUX_PKG_VERSION.zip
+TERMUX_PKG_DEPENDS="gtk3, gstreamer"
