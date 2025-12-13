c11e8b790373bbaab7f5a685c4866d4cc609959a
alexdvn <alexdgames01@gmail.com>
2025-12-13 10:29:49 -0500

    Add build steps for ZMusic package

--- packages/zmusic/build.sh
+++ packages/zmusic/build.sh
@@ -8,3 +8,34 @@ TERMUX_PKG_SHA256=f49f3ac3d4845304f45f485f5dd11095504caaf9f1c26b4995b45b1582ee03
 TERMUX_PKG_DEPENDS="fluidsynth, game-music-emu, libmpg123, libsndfile"
 TERMUX_PKG_BUILD_DEPENDS="clang, make, pkg-config, cmake"
 
+termux_step_pre_configure() {
+    # Some projects require creating the build directory
+    # or preparing the source before configuration.
+    # ZMusic might need a clean build directory.
+    mkdir -p $TERMUX_PKG_BUILDDIR
+}
+
+termux_step_configure() {
+    # Use CMake to configure the project.
+    # You might need to adjust the flags based on ZMusic's requirements.
+    # -DCMAKE_INSTALL_PREFIX is crucial for Termux installation path.
+    # -DCMAKE_BUILD_TYPE=Release is common for optimized builds.
+    cmake -G "Unix Makefiles" \
+        -S $TERMUX_PKG_SRCDIR \
+        -B $TERMUX_PKG_BUILDDIR \
+        -DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
+        -DCMAKE_BUILD_TYPE=Release \
+        -Wno-dev
+}
+
+termux_step_make() {
+    # Use 'make' to compile the project in the build directory.
+    # -j $TERMUX_MAKE_PROCESSES is standard for parallel compilation.
+    make -C $TERMUX_PKG_BUILDDIR -j $TERMUX_MAKE_PROCESSES
+}
+
+termux_step_make_install() {
+    # Use 'make install' to copy the compiled files to the final destination.
+    make -C $TERMUX_PKG_BUILDDIR install
+}
+
