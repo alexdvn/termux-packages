435d241ff04f602c27da87432a8a50fc827f2315
alexdvn <alexdgames01@gmail.com>
2025-12-14 13:05:59 -0500

    Make a half-assed build script becuase github decided to be retarded when I copied the srcurl

---
+++ x11-packages/lzdoom/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/drfrag666/lzdoom
+TERMUX_PKG_DESCRIPTION="GZDoom but with Doom Legacy support, now forked under a!"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER=@alexdvn
+TERMUX_PKG_VERSION=4.13.3a
+
